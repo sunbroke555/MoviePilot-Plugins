e8d6575d93073ee32c9a77d29c6308db8f0b9d9c
sunbroke <74763419+sunbroke555@users.noreply.github.com>
2026-03-27 22:55:40 +0800

    Update __init__.py

--- plugins/cloudassistant/__init__.py
+++ plugins/cloudassistant/__init__.py
@@ -65,7 +65,7 @@ class CloudAssistant(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/thsrite/MoviePilot-Plugins/main/icons/cloudassistant.png"
     # 插件版本
-    plugin_version = "2.1.6"
+    plugin_version = "2.3.6"
     # 插件作者
     plugin_author = "thsrite"
     # 作者主页
